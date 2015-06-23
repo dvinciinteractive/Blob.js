d68c7cd4a327ecf86b2ecd758d1db5ea61018d9f
Jim Auldridge <auldridgej@gmail.com>
2015-06-23 10:37:13 -0400

    add build file

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+/*jslint node: true */
+(function () {
+    'use strict';
+
+    var gulp = require('gulp'),
+        del = require('del'),
+        plugins = require('gulp-load-plugins')();
+
+    gulp.task('clean', function (cb) {
+        del(['dist'], cb);
+    });
+
+    gulp.task('lint', function () {
+        return gulp.src('src/**/*.js')
+            .pipe(plugins.jslint());
+    });
+
+    gulp.task('default', ['lint', 'clean'], function () {
+        /*
+         * Load in the JS file
+         */
+        return gulp.src('src/**/*.js')
+            /*
+             * Uglify (minify) it
+             */
+            .pipe(plugins.uglify())
+            /*
+             * Annotate that it was minified by adding .min to filename
+             */
+            .pipe(plugins.rename({
+                suffix: '.min'
+            }))
+            /*
+             * Output to our distribution location
+             */
+            .pipe(gulp.dest('dist'));
+    });
+}());
